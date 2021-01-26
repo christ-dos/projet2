078a4cff6e5296b02083738a92831dc707f3a036
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-01-26 23:28:40 +0100

    javaDoc updated
     generate javaDoc
    
     javadoc file javadoc.xml and doc/com

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"services"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.hemebiotech.analytics"},{"l":"controller"},{"l":"services"}];updateSearchResults();
